cd1a4adfed93a2dcb89aab90341ea2a9d83c256d
Alex <alexandrkibalka@gmail.com>
2023-10-13 00:50:38 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,15 @@
 
 const categories = document.getElementById('categories');
 
-const list = categories.querySelectorAll(".item");
-
+const collection = categories.children;
+const list = Array.from(collection);
 console.log(`Number of categories: ${list.length}`);
 
 list.forEach((category) => {
-  const title = category.querySelector("h2");
-  const categoryItems = category.querySelectorAll("li");
+  const title = category.children[0];
+  const categoryItems = category.lastElementChild.childElementCount;
   console.log(`Category: ${title.textContent}`);
-  console.log(`Elements: ${categoryItems.length}`);
+  console.log(`Elements: ${categoryItems}`);
 });
 
 
