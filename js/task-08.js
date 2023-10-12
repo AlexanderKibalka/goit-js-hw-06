e948674c6174eac5a1e4cdffbbe186dc8f8a627a
Alex <alexandrkibalka@gmail.com>
2023-10-12 20:58:47 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', hendlerSubmit);
+function hendlerSubmit(evt) {
+
+    evt.preventDefault();
+
+    const { password, email } = evt.currentTarget.elements;
+
+    if (!password.value || !email.value) {
+        alert("Усі поля повинні бути заповнені!");
+        return;
+    }
+
+    const date = {
+        password: password.value,
+        email: email.value,
+    };
+
+    console.log(date);
+
+    evt.currentTarget.reset();
+}
