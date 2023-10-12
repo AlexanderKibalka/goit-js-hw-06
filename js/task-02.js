f7e403f74f65b4cc9102d67c7ba7a31ddc0d41ea
Alex <alexandrkibalka@gmail.com>
2023-10-12 10:31:08 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const container = document.getElementById('ingredients');
+
+
+const ingredientItems = ingredients.map((ingredient) => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+container.append(...ingredientItems);
+console.log(container);
+
